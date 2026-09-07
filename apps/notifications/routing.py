cc204c5a39c43e65f581201916f1b37d74914721
Emil Mardanov <70972751+sonnygalyeon@users.noreply.github.com>
2026-09-07 09:21:26 +0300

    feat(1.1.1): route notification websocket

---
+++ apps/notifications/routing.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from apps.notifications.consumer import NotificationConsumer
+
+
+websocket_urlpatterns = [
+    path("ws/notifications/", NotificationConsumer.as_asgi()),
+]
