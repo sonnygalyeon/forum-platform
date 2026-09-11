34d64ea029f7289cb688b847601b709c796f7acb
Emil Mardanov <70972751+sonnygalyeon@users.noreply.github.com>
2026-09-11 13:54:55 +0300

    feat(1.1.6): register engagement signals

--- apps/social/apps.py
+++ apps/social/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
 
+
 class SocialConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.social"
+
+    def ready(self):
+        from apps.social import signals  # noqa: F401
