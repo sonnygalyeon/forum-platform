9ebef901e75d20d1f108149810c94814a0136057
Emil Mardanov <70972751+sonnygalyeon@users.noreply.github.com>
2026-09-11 08:47:28 +0300

    fix(1.1.2): import graph query predicates

--- apps/social/api/views.py
+++ apps/social/api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import generics, serializers, status
