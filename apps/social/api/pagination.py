3f571d7ac44236f60e3e400788094b848c659fcd
Emil Mardanov <70972751+sonnygalyeon@users.noreply.github.com>
2026-09-11 08:46:34 +0300

    feat(1.1.2): add social graph pagination

--- apps/social/api/pagination.py
+++ apps/social/api/pagination.py
@@ -1,6 +1,12 @@
-from rest_framework.pagination import CursorPagination
+from rest_framework.pagination import CursorPagination, PageNumberPagination
 
 
 class PersonalizedFeedCursorPagination(CursorPagination):
     page_size = 20
     ordering = ("-feed_score", "-created_at", "-id")
+
+
+class SocialGraphPageNumberPagination(PageNumberPagination):
+    page_size = 20
+    page_size_query_param = "page_size"
+    max_page_size = 50
